f0d330c2819631de060f78fb461f1c40f460d8d1
HazemAhmedSaad <zozahmed939@gmail.com>
2023-11-30 18:46:22 +0200

    add style of Book Details page

--- src/Components/BookDetails/BookDetails.js
+++ src/Components/BookDetails/BookDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./BookDetails.css";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -19,61 +19,56 @@ const BookDetails = () => {
   }, [id]);
   return (
     <div>
-      <div>
-        <div>
-          <img
-            className="photo-details"
-            src={book.image_url}
-            alt="book"
-          />
+      <div className="row mx-auto my-5 pt-4">
+        <div className="col-lg-3 col-md-4 col-sm-10 mx-auto text-center pb-4">
+          <img className="photo-details" src={book.image_url} alt="book" />
+        </div>
+        <div className="col-lg-9 col-md-8 col-sm-10 mx-auto ps-4 book-info">
+          <h2 className="head-title">{book.title}</h2>
+          <p className="p-info"><Link  to={`/search/${book.authors}`}>{book.authors}</Link></p>
+          <p className="p-info">{book.genre_list}</p>
+          <ul className="nav book-table-info align-items-center">
+            <li>
+              <p>Pages</p>
+              <p>
+                <span>{book.num_pages}</span>
+              </p>
+            </li>
+            <li>
+              <p>Language</p>
+              <p>
+                <span>English</span>
+              </p>
+            </li>
+            <li>
+              <p>Rating</p>
+              <p>
+                <span>{book.rating}</span>
+              </p>
+            </li>
+          </ul>
+          <ul className="nav book-table-info align-items-center">
+            <li>
+              <p>Rating Count</p>
+              <p>
+                <span>{book.rating_count}</span>
+              </p>
+            </li>
+            <li>
+              <p>Review Count</p>
+              <p>
+                <span>{book.review_count}</span>
+              </p>
+            </li>
+          </ul>
         </div>
       </div>
-      <div>
-        <h2>{book.title}</h2>
-        <p>{book.authors}</p>
-        <p>{book.genre_list}</p>
-        <ul>
-          <li>
-            <p>Pages</p>
-            <p>
-              <span>{book.num_pages}</span>
-            </p>
-          </li>
-          <li>
-            <p>Language</p>
-            <p>
-              <span>English</span>
-            </p>
-          </li>
-          <li>
-            <p>Rating</p>
-            <p>
-              <span>{book.rating}</span>
-            </p>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <p>Rating Count</p>
-            <p>
-              <span>{book.rating_count}</span>
-            </p>
-          </li>
-          <li>
-            <p>Review Count</p>
-            <p>
-              <span>{book.review_count}</span>
-            </p>
-          </li>
-        </ul>
+
+      <div className="description">
         <div>
-            <div>
-                <p>
-                    <strong>
-                        {book.description}
-                    </strong>
-                </p>
-            </div>
+          <p>
+            <strong>{book.description}</strong>
+          </p>
         </div>
       </div>
     </div>
